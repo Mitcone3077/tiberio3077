8ea35152d0301d248c921a445b88118ee1072450
Mitcone3077 <mtiberio.art@gmail.com>
2021-09-28 12:42:38 -0300

    arte-4 tweakpane02

--- src/modules/arte-4/js/index.js
+++ src/modules/arte-4/js/index.js
@@ -1,7 +1,7 @@
 import canvasSketch from "canvas-sketch";
 import { random, math, color } from "canvas-sketch-util";
 import { Pane } from 'tweakpane';
-import style from "../css/style.css";
+//import style from "../css/style.css";
 
 
 const settings = {
