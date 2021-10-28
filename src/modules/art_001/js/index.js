5055aa6a7af4ccf4eb57f9670b34a0e6cafc966e
Bruno Talanski <brn.talanski@gmail.com>
2021-10-28 11:22:26 -0300

    Refactor art_001 js code

--- src/modules/art_001/js/index.js
+++ src/modules/art_001/js/index.js
@@ -1,127 +1,87 @@
 import p5 from "p5";
+import { Vector } from "p5";
 import style from "../css/style.css";
-import { Pane } from 'tweakpane';
-
+import { Pane } from "tweakpane";
 
 // The Chaos Game
 
-
-let points;
-let current;
-let percent = 0.5;
-let previous;
-var n = 8;
-var m = 3;
-var rColor = 100;
-var gCaolor = 100;
-var bColor = 100;
-
-
-
-const init = (p) =>{          //========================INITIALIZATION
+// Using p5 in instance mode
+// Video: https://www.youtube.com/watch?v=Su792jEauZg&ab_channel=TheCodingTrain
+// read more: https://github.com/processing/p5.js/wiki/p5.js-overview#instantiation--namespace
+const init = (p5Instance) => {
+  // Keep your variables scoped locally instead of globally
+  // Keep your variables declarion consistant instead of mixing vars, lets and consts
+  let points;
+  let current;
+  let percent = 0.5;
+  let previous;
+  let n = 8; // Use more descriptive names
+  let m = 3; // Use more descriptive names
+  let rColor = 100;
+  let gColor = 100;
+  let bColor = 100;
   
-  function reset() {
+  // Reset 
+  const reset = () => {
     for (let i = 0; i < n; i++) {
-      // let v = createVector(random(width), random(height));
-      let angle = i * p.TWO_PI / m;
-      let v = p.Vector.fromAngle(angle);
-      v.mult(p.width / 8);
-      v.add(p.width / 2, p.height / 2);
+      let angle = (i * p5Instance.TWO_PI) / m;
+
+      // fromAngle() is a static method from the p5.Vector class.
+      // read more: https://p5js.org/reference/#/p5.Vector
+      let v = p5.Vector.fromAngle(angle);
+      v.mult(p5Instance.width / 8);
+      v.add(p5Instance.width / 2, p5Instance.height / 2);
       points.push(v);
     }
-    
-    current = p.createVector(p.random(p.width), p.random(p.height));
-    
-    p.stroke(255,0,0,255);
-    p.strokeWeight(10);
-    for (let p of points) {
-      p.point(p.x, p.y);
-    }
+
+    current = p5Instance.createVector(
+      p5Instance.random(p5Instance.width),
+      p5Instance.random(p5Instance.height)
+    );
+
+    p5Instance.stroke(255, 0, 0, 255);
+    p5Instance.strokeWeight(10);
+
+    points.forEach((point) => p5Instance.point(p5Instance.point(point.x, point.y)))
   }
 
+  // Setup function
+  p5Instance.setup = function() {
+    p5Instance.createCanvas(p5Instance.windowWidth, p5Instance.windowHeight);
+    p5Instance.colorMode(p5Instance.RGB, 255);
+    points = [];
+    p5Instance.background(51);
 
-  p.setup = function setup() {//========================SETUP FUNCTION
+    reset();
+  };
 
-  p.createCanvas(p.windowWidth, p.windowHeight);
-  p.colorMode(p.RGB,255);
-  points = [];
-  p.background(51);
-
-  
-  reset();
-
-
-
-   } //===END OF SETUP
-
-
-  
-
-
-  p.draw = function draw() {//============================DRAW FUNCTION
-
-
-    if (frameCount % 200 == 0) {
+  // Draw function
+  p5Instance.draw = function() {
+    if (p5Instance.frameCount % 200 == 0) {
       m += 1;
       percent = 0;
       rColor += 20;
-      p.reset();
+      reset();
     }
 
     for (let i = 0; i < 10000; i++) {
-      p.strokeWeight(0.3);
-      p.stroke(rColor,gColor,bColor,5);
-      let next = p.random(points);
-      if (next !== previous) {
-        current.x = p.lerp(current.x, next.x, percent);
-        current.y = p.lerp(current.y, next.y, percent);
-        p.point(current.x, current.y);
-        percent+= 0.000001;
-      }
+      p5Instance.strokeWeight(0.3);
+      p5Instance.stroke(rColor, gColor, bColor, 5);
+      let next = p5Instance.random(points);
       
+      if (next !== previous) {
+        current.x = p5Instance.lerp(current.x, next.x, percent);
+        current.y = p5Instance.lerp(current.y, next.y, percent);
+        p5Instance.point(current.x, current.y);
+        percent += 0.000001;
+      }
+
       previous = next;
     }
+  };
+};
 
-   } //===END OF DRAW
-
-
-
-
-
-
- }; //===END OF INIT
-
-
-new p5 (init,document.getElementById("p5")); //== RUN >
-
-
-
-
-/* ===================== TO REFACTOR  =================
-
-
-
-
-
-
-
-function setup() {
-  
-}
-
-
-
-function draw() {
-
-
-
-
-}
-
-
-
-
-
-
-
-================================= End of Code ====================================*/
+// Using p5 in instance mode
+// Video: https://www.youtube.com/watch?v=Su792jEauZg&ab_channel=TheCodingTrain
+// read more: https://github.com/processing/p5.js/wiki/p5.js-overview#instantiation--namespace
+new p5(init, document.getElementById("p5"));
